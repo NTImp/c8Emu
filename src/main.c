30f694ef15e7709b22a4e0f5ea572beb60f0a99f
Jose Merono <josesmerono@gmail.com>
2017-02-06 17:27:51 +0100

    .

--- src/main.c
+++ src/main.c
@@ -8,10 +8,12 @@
 #define EmulatorRunning 1
 #define EmulatorPaused 0
 #define EmulatorStop -1
+#define MaxSpeed 10
 
 void PollEvents();
 void InitEmulator();
 
+char speed = MaxSpeed - 1;
 char quit = 0;
 char emulator_status = EmulatorStop;
 const char* rom = 0;
@@ -19,41 +21,41 @@ const char* rom = 0;
 struct chip8 machine;
 
 int main(int argc, char** argv) {
-    if (argc >= 1) {
-        rom = argv[1];
-    }
+	if (argc >= 1) {
+	rom = argv[1];
+	}
 
-    if(!InitSDL())
-        return -1;
+	if(!InitSDL())
+	return -1;
 
-    InitEmulator();
+	InitEmulator();
 
-    Uint32 clock_ticks = SDL_GetTicks();
-    Uint32 op_ticks = SDL_GetTicks();
-    while(!quit) {
-        PollEvents();        
+	Uint32 clock_ticks = SDL_GetTicks();
+	Uint32 op_ticks = SDL_GetTicks();
+	while(!quit) {
+		PollEvents();        
 
-        if (emulator_status == EmulatorRunning) {
-            if(SDL_GetTicks() - clock_ticks > 1000/60) {
-                update_clock(&machine);
-                clock_ticks = SDL_GetTicks();
-            }
+		if (emulator_status == EmulatorRunning) {
+			if(SDL_GetTicks() - clock_ticks > 1000/60) {
+				update_clock(&machine);
+				clock_ticks = SDL_GetTicks();
+			}
 
-            if (SDL_GetTicks() - op_ticks > 2) {
-                C8Updatekeys(machine.keys);
-                chip8_update(&machine, 1);
+			if (SDL_GetTicks() - op_ticks > MaxSpeed - speed) {
+				C8Updatekeys(machine.keys);
+				chip8_update(&machine, 1);
 
-                FillScreen(machine.screen);
-                DrawScreen();
+				FillScreen(machine.screen);
+				DrawScreen();
 
-                op_ticks = SDL_GetTicks();
-            }
-        }
-    }
+				op_ticks = SDL_GetTicks();
+			}
+		}
+	}
 
-    ExitSDL();
+	ExitSDL();
 
-    return 0;
+	return 0;
 }
 
 void InitEmulator() {
@@ -73,22 +75,31 @@ void PollEvents() {
         char key;
         switch (ev.type) {
         case SDL_QUIT:
-            quit = 1;
-            break;
+		quit = 1;
+		break;
         case SDL_KEYDOWN:
             key = ev.key.keysym.scancode;
             if(!ev.key.repeat) {
                 switch (key) {
                 case SDL_SCANCODE_RETURN:
-                    if (emulator_status == EmulatorRunning) {
-                        emulator_status = EmulatorPaused;
-                    } else if (emulator_status == EmulatorPaused) {
-                        emulator_status = EmulatorRunning;
-                    }
-                    break;
-                case SDL_SCANCODE_SPACE:
-                    InitEmulator();
-                }
+			if (emulator_status == EmulatorRunning) {
+			emulator_status = EmulatorPaused;
+			} else if (emulator_status == EmulatorPaused) {
+			emulator_status = EmulatorRunning;
+			}
+			break;
+                case SDL_SCANCODE_ESCAPE:
+			InitEmulator();
+			break;
+		case SDL_SCANCODE_UP:
+			speed = (speed + 1) % MaxSpeed;	
+			printf("Speed is: %d/9\n");
+			break;
+		case SDL_SCANCODE_DOWN:
+			speed = (speed + MaxSpeed - 1) % MaxSpeed;	
+			printf("Speed is: %d/9\n");
+			break;
+		}
             }
             break;
         }
